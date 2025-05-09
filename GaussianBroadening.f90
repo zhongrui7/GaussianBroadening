a292c077999872112d341c2ae1f1ac4c2a63f7f6
zhongrui7 <38693476+zhongrui7@users.noreply.github.com>
2025-05-09 11:57:33 -0400

    Update GaussianBroadening.f90

--- GaussianBroadening.f90
+++ GaussianBroadening.f90
@@ -4,21 +4,21 @@ PROGRAM GaussianBroadening
 !
 ! Purpose : Perform a Gaussian broadening on a set of impulse signal
 ! data as input.
-!           Sigma will be calculated from the height of the impulses  signal.
+!           
 !           You must input the range and the step of the output data in
 !           the proper order.
 !           No output file to be written, since it is designed to output
-!           to standard screen
-!           so as to work with gnuplot, as usage shown below.
-!           if you want to get an output file with final data,
-!           uncomment related 2 lines.
-!           you would get 'g-broadened.dat'
-! Usage:  ./command input_file min max step sigma
-! UsageEx : plot '< gbroad.x input.dat width Xmin Xmax Xstep < ' u 1:2
+!           to the standard screen to work with gnuplot
+!           
+!           If you want to get an output file with final data,
+!           uncomment the related 2 lines.
+!           You would get 'g-broadened.dat'
+! Usage   :  ./command input_file min max step sigma
+! UsageEx :  plot '< gbroad.x input.dat width Xmin Xmax Xstep < ' u 1:2
 ! ...
-!           in which, width     : FWHM, full width at half maximum,        e.g., 3.0
-!                     Xmin, Xmax: the range of X in output, e.g., 500          3000
-!                     Xstep     : the increment of X in the output,  e.g., 2
+!           in which, sigma     : the standard deviation of the Gaussian distribution is ⁠σ⁠ (sigma)
+!                     Xmin, Xmax: the range of X in the output
+!                     Xstep     : the increment of X in the output
 !------------------------------------------------------------------------------------------------------------
 !
 implicit none
@@ -28,7 +28,7 @@ character(len=20)      :: arg, input
 character(len=100)     :: temp
 real(dp), allocatable  :: X0(:), Y0(:) ! 0 - input, no 0 - output
 integer,parameter      :: inputfile = 10, outputfile = 20
-real(dp) ::  X, Y, Xmin, Xmax, Ysum, Yavg, sigma, step, pi ! sigma2 is sigmasquare
+real(dp) ::  X, Y, Xmin, Xmax, sigma, step, pi 
 integer  :: i, j, nline, stat  ! nline: number of lines
 logical  :: alive
 
@@ -51,7 +51,7 @@ read(arg, *) step
 call getarg(5, arg)
 read(arg, *) sigma
 
-! open and count number of lines in input file
+! Open and count the number of lines in the input file
 open(unit=inputfile, file=input, access="sequential", status="old")
 
 nline = 0
@@ -66,7 +66,7 @@ rewind(inputfile)
 ! allocate memory for arrays X0, Y0
 allocate(X0(1:nline), Y0(1:nline))
 
-! read in data from input file
+! Read in data from the input file
 do i = 1, nline
      read(unit=inputfile,FMT=*,iostat=stat) X0(i), Y0(i)
 end do
@@ -74,11 +74,11 @@ end do
 ! Uncomment the following 7 lines if you want to export the broadening result to a file
 ! open(unit=outputfile,file="g-broadended.dat", status='replace',  action='write')
 ! write(outputfile,*) "./command input_file Xmin Xmax step sigma"
-! write(outputfile,*) " inputfile= ", input
-! write(outputfile,*) " Xmin= ", Xmin
-! write(outputfile,*) " Xmax= ", Xmax
-! write(outputfile,*) " step= ", step
-! write(outputfile,*) " sigma= ",sigma
+! write(outputfile,*) " inputfile = ", input
+! write(outputfile,*) " Xmin = ", Xmin
+! write(outputfile,*) " Xmax = ", Xmax
+! write(outputfile,*) " step = ", step
+! write(outputfile,*) " sigma = ",sigma
 
 pi = 2.0 * acos(0.0_dp)
 
@@ -91,7 +91,7 @@ do while(X .le. Xmax)
              /(2.0*sigma*sigma) )
        end if
      end do
-   ! uncomment the following line if you want to export the broadening result to a file
+   ! Uncomment the following line if you want to export the broadening result to a file
    !  write(unit=outputfile,fmt="(F9.1,1X,F15.8)") X, Y
      write(*,fmt="(F9.1,1X,F15.8)") X, Y
      X = X + step
